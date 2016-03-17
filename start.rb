a6872c2c58e2c3bb3ab0e35ffa10d85742881eef
Otavio Pereira (Ramal 27) <otavio@XYZTEMAS.local>
2016-03-17 18:29:33 -0300

    Aprendizados do Livro Casa do Codigo cap. 01

--- start.rb
+++ start.rb
@@ -1,12 +1,9 @@
 #coding: utf-8
 
-array_a = []
-
-array_a.push(10)
-array_a.push("Ola")
-array_a.push(10)
-array_a.push("Oi")
-
-array_a.each do |i|
-  puts i
+class String
+  def plural
+    "#{self}s"
+  end
 end
+
+puts "caneta".plural
