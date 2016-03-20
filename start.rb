f5986966937fb071f891f09bd10b118178e6a084
Otavio <otajesus@bol.com.br>
2016-03-20 14:03:26 -0300

    Escapes Ruby 02

--- start.rb
+++ start.rb
@@ -1,5 +1,11 @@
 string_especial = "Isso é \"Normal\" e \'Util\' No mundo Ruby"
 
-string_especial = %{Isso é "Normal" e 'Util' no mundo Ruby} #%<>, %[]
+string_especial = %{Isso é "Normal" e {Util} no mundo Ruby} #%<>, %[]
+
+string_especial = %{Isso é "Normal" e 'Util'
+                    no mundo Ruy} #Escape com quebra de linha
+
+numero = 10
+string_especial = %{Isso é "Normal" e 'Util' no mundo Ruy #{numero}} # Com Interpolação
 
 puts string_especial
