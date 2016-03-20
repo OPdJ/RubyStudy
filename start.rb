c14ffac136f8bd8554a48e8ee9bc16001837e2ea
Otavio <otajesus@bol.com.br>
2016-03-20 00:00:51 -0300

    EE

--- start.rb
+++ start.rb
@@ -1,5 +1,5 @@
 string_especial = "Isso é \"Normal\" e \'Util\' No mundo Ruby"
 
-string_especial = %{Isso é "Normal" e 'Util' no mundo Ruby} #%<>, %[]
+string_especial = %{Isso é "Normal" e 'Util' no mundo Ruby} #%<>, %[], %~~
 
 puts string_especial
